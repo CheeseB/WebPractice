4766507b55eb5e5f9a199364842574ab6313bc7b
CheeseB <2489ckckck@naver.com>
2020-07-29 00:17:34 +0900

    modified

--- JSpractice/zerocho.js
+++ JSpractice/zerocho.js
@@ -15,12 +15,12 @@ var result = document.createElement('div');
 body.append(result);
 
 button.addEventListener('click', function callback() {
-    // 콜백함수 이름은 생략 가능
-    if (word.textContent[word.textContent.length - 1] === input.value[0]) {
-        result.textContent = '오예';
-        word.textContent = input.value;
-        input.value = '';
-    } else {
-        result.textContent = '으엑';
-    }
+	// 콜백함수 이름은 생략 가능
+	if (word.textContent[word.textContent.length - 1] === input.value[0]) {
+		result.textContent = '오예';
+		word.textContent = input.value;
+		input.value = '';
+	} else {
+		result.textContent = '으엑';
+	}
 });
